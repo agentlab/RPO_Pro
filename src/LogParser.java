e74e5adbd80b30bcb9bfcce8c5e47413c8cdc226
Marat Agapov <magapov95@gmail.com>
2017-12-13 01:50:20 +0300

    HDFS write serialized suricata logs works.

--- src/LogParser.java
+++ src/LogParser.java
@@ -3,12 +3,11 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.commons.lang.SerializationUtils;
 
 class LogParser {
 	public static void main(String args[]) throws IOException {
@@ -18,47 +17,32 @@ class LogParser {
 		
 		ArrayList<HashMap> eventsList = fw.read("/var/log/suricata/eve.json");
 		
-		String hdfsPath = "/usr/local/hadoop/tmp/hdfs/";
-		
-		String fileName = "suricataLog.bin";
-		
-		String nameNode = "hdfs://localhost:8020";
-		
-		String content = "SoneTextInFile";
+		String hdfsURI = "hdfs://localhost:9000";
+		String path = "/suricataLog";
+		String fileName = "log.bin";
 		
 		Configuration conf = new Configuration();
-		conf.set("fs.defaultFS", nameNode);
+		conf.set("fs.defaultFS", hdfsURI);
+		
 		conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
-		conf.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
-		
-		System.setProperty("HADOOP_USER_NAME", "hduser");
-		System.setProperty("hadoop.home.dir", "/");
-		
-		FileSystem fs = FileSystem.get(URI.create(nameNode), conf);
-		Path workingDir = fs.getWorkingDirectory();
-		Path newFolderPath = new Path(hdfsPath);
-		
-		if(!fs.exists(newFolderPath)) {
+	    conf.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
+	    
+	    System.setProperty("HADOOP_USER_NAME", "hduser");
+	    System.setProperty("hadoop.home.dir", "/");
+	    
+	    FileSystem fs = FileSystem.get(URI.create(hdfsURI), conf);
+	    
+		Path newFolderPath = new Path(path);
+		if (fs.exists(newFolderPath)) {
+			fs.delete(newFolderPath, true);
 			fs.mkdirs(newFolderPath);
 		}
 		
-		Path hdfsWritePath = new Path(newFolderPath + "/" +fileName);
+		Path hdfswritepath = new Path(newFolderPath + "/" + fileName);
+		FSDataOutputStream outputStream = fs.create(hdfswritepath);
 		
-		FSDataOutputStream ofStream = fs.create(hdfsWritePath);
-		
-		ofStream.writeBytes(content);
-		ofStream.close();
-		
-		//End of writing
-		//Begin of reading
-		Path hdfsReadPath = new Path(newFolderPath + "/" +fileName);
-		
-		FSDataInputStream ifStream = fs.open(hdfsReadPath);
-		String readStr = IOUtils.toString(ifStream, "UTF-8");
-		ifStream.close();
-		fs.close();
-		
-		System.out.println(readStr);
+		SerializationUtils.serialize(eventsList, outputStream);
+		outputStream.close();
 	}
 }
 
