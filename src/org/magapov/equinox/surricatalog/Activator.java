dd1c1debb07624951abedb1aee669796a6f0bed1
magapov <magapov95@gmail.com>
2017-12-15 01:13:00 +0300

    New activator

--- src/org/magapov/equinox/surricatalog/Activator.java
+++ src/org/magapov/equinox/surricatalog/Activator.java
@@ -5,15 +5,18 @@ import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator{
 	private FileWorker fw; 
+	private Thread myThread;
 	@Override
 	public void start(BundleContext context) throws Exception {
 		System.out.println("Bundle started");
 		fw = new FileWorker();
+		myThread = new Thread(fw);
+		myThread.start();
 	}
 
 	@Override
 	public void stop(BundleContext context) throws Exception {
-		fw.interrupt();
+		myThread.interrupt();
 		System.out.println("Bundle stopped");
 	}
 	
