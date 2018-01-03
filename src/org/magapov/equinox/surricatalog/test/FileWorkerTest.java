64ab30ff75ca52a94d83970ef131a8fe203d939c
magapov <magapov95@gmail.com>
2018-01-04 01:32:34 +0300

    slf4j multiple bindings error

--- src/org/magapov/equinox/surricatalog/test/FileWorkerTest.java
+++ src/org/magapov/equinox/surricatalog/test/FileWorkerTest.java
@@ -7,13 +7,45 @@ import static org.junit.Assert.*;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import org.junit.FixMethodOrder;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runners.Suite;
-import org.junit.runners.MethodSorters;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.verify;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.core.Appender;
 
 
+@RunWith(MockitoJUnitRunner.class)
 public class FileWorkerTest {
+	@Mock
+	private Appender mockAppender;
+	
+	@Captor
+	private ArgumentCaptor<LoggingEvent> captorLoggingEvent;
+	
+	@Before
+	public void setup() {
+        final Logger logger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        logger.addAppender(mockAppender);
+    }
+	
+	@After
+    public void teardown() {
+        final Logger logger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        logger.detachAppender(mockAppender);
+    }
+	
 	@Test
 	public void EmptyTest() {
 		assertTrue(true);
@@ -24,6 +56,11 @@ public class FileWorkerTest {
 		FileWorker fw = new FileWorker("/home/dts/123123123.dat");
 		Thread th = new Thread(fw);
 		th.start();
+		
+		verify(mockAppender).doAppend(captorLoggingEvent.capture());
+        final LoggingEvent loggingEvent = captorLoggingEvent.getValue();
+        assertThat(loggingEvent.getLevel(), is(Level.INFO));
+        assertThat(loggingEvent.getMessage(), is("FileNotFoundException throws"));
 	}
 	
 	@Test(expected = IOException.class)
